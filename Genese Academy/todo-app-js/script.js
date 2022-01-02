7bf4a36e09f3f01f83cc95b966e903cc77117317
dilli <dillihangrae@gmail.com>
2022-01-02 17:53:00 +0545

    initial commit

--- Genese Academy/todo-app-js/script.js
+++ Genese Academy/todo-app-js/script.js
@@ -110,39 +110,141 @@
 // // how to extract the data of console.log(notes)
 // // --> use map, or for loop
 
-let MY_NOTE='_my_note';
-let my_notes =  [];
+// let MY_NOTE = '_my_note';
+// // fetching the pre data so that user can see the previously saved data
+// // ternary operator as in a form of if else statment here
+// let my_notes = localStorage.getItem(MY_NOTE)?
+//     JSON.parse(
+//         localStorage.getItem(MY_NOTE)): [] ;
+// // english months
+// let eng_month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+// window.onload=function () {
+//   onGetSavedNotes();
+// };
 
 
-window.onload=function () {
-  onGetSavedNotes(my_notes);
-};
+// function onAddNote() {
+//     let myNote = document.getElementById('note').value;
+//     let note = {};
+//     note.value = myNote;
+//     note.date = new Date();
+//     note.id = new Date().getTime();
 
-function onAddNote() {
-    let myNote=
-        document.
-        getElementById('note').value;
-    let note={};
-    note.value=myNote;
-    note.date=new Date();
-    note.id=new Date().getTime();
+//     my_notes.push(note);
+//     localStorage.setItem(MY_NOTE,JSON.stringify(my_notes));
+// }
 
-    my_notes.push(note);
-    localStorage.setItem(MY_NOTE,JSON.stringify(my_notes));document.getElementById('note').value='';
-    onGetSavedNotes(my_notes);
+// function onGetSavedNotes() {
+//     let notes = localStorage.getItem(MY_NOTE);
+//     let my_notes_html='';
+//     JSON.parse(notes).forEach(function (val, index) {
+//         if (val){
+//         my_notes_html = my_notes_html +
+//         // + '<div> ' + 
+//         // '<p> ' +  val.date + '</p> ' +
+//         // '<p> ' +  val.value + '</p>' +
+//         // + '</div>'; / 
+//         // $ will tel the browser the to extract any variable it's like quotation in Js
+//         `<div class="del"> <p> ${getUserReadableDate(val.date)} </p>
+//         <p> ${val.value} </p>
+//         <p class="delete" onclick="deleteNote(` + index + `)""> Delete </p>
+//         </div>`
+//      } }); 
+//     document.getElementById('myNotes').innerHTML = my_notes_html;
+// }
 
-    console.log(my_notes)
-}
 
-function onGetSavedNotes(notes) {
-    let my_notes_html='';
-    notes.forEach(function (val) {
-        my_notes_html = my_notes_html + '<div> ' + '<p> This is Note </p> ' + '<p> Date </p>' + '</div>';
-        console.log(val)
-    });
-    document.getElementById('myNotes').innerHTML=my_notes_html;
+// // get User Readable Date
+// function getUserReadableDate(date){
+//     let readableDate = new Date(date);
+//     // date format 02 Jan 2021 5:06PM
+//     return readableDate.getDate() + ' '
+//     + eng_month [readableDate.getMonth()] + ' / ' +
+//     readableDate.getFullYear() + '  ' + ' Time is ' + readableDate.getHours() + ':'
+//     + readableDate.getMinutes();
+    
+// }
 
-}
+
+// function deleteNote(index){
+//     console.log(index)
+//     delete my_notes[index];
+//     localStorage.setItem(MY_NOTE, JSON.stringify(my_notes));
+//     onGetSavedNotes();
+// }
+
+
+
+// ---- Written By Ashish Gautam React js Teacher------
+// let MY_NOTE='_my_note';
+// let eng_month=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+// let my_notes=localStorage.getItem(MY_NOTE)?
+//     JSON.parse(
+//     localStorage.getItem(MY_NOTE)):[];
+// window.onload=function () {
+//   onGetSavedNotes(my_notes);
+// };
+// function onAddNote() {
+//     let myNote=
+//         document.
+//         getElementById('note').value;
+//     let note={};
+//     note.value=myNote;
+//     note.date=new Date();
+//     note.id=new Date().getTime();
+//     my_notes.push(note);
+//     localStorage.setItem(MY_NOTE,
+//         JSON.stringify(my_notes));
+//     document.
+//     getElementById('note').value='';
+//     onGetSavedNotes(my_notes);
+// }
+
+// function onGetSavedNotes(notes) {
+//     let my_notes_html='';
+//     notes.forEach(function (val,index) {
+//         if(val) {
+//             my_notes_html = my_notes_html +
+//                 `<div>
+//                  <p>${getUserReadableDate(val.date)}</p>
+//                 <p>${val.value}</p>
+//                 <div class="delete" onclick="deleteNote(` + index + `)">Delete</div>
+//                 </div>`
+//         }
+//     });
+//     document.getElementById('myNotes')
+//         .innerHTML=my_notes_html;
+
+
+// }
+
+// function getUserReadableDate(date) {
+//     let readableDate=new Date(date);
+//    // 11 NOv 2019 2:30
+//     return readableDate.getDate()+' '+eng_month[readableDate.getMonth()]
+//     +' '+readableDate.getFullYear()+' '+readableDate.getHours()+':'
+//     +readableDate.getMinutes();
+
+// }
+// function deleteNote(index) {
+//     delete my_notes[index];
+//     localStorage.setItem(MY_NOTE,JSON.stringify(my_notes));
+//     onGetSavedNotes(my_notes);
+
+// }
+
+// function onSearchItem() {
+// let search_key=document.getElementById('searchText').value;
+// let newArray=my_notes.filter(
+//     note=>note.value.toLowerCase().indexOf(search_key)!=-1);
+//     onGetSavedNotes(newArray);
+// console.log(my_notes);
+// }
+// function clearSearch() {
+//     onGetSavedNotes(my_notes);
+// }
+
 
 
 
