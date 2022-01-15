2704f57d5a38de6e3d98899c55c11d9256069944
dilli <dillihangrae@gmail.com>
2022-01-15 22:05:23 +0545

    initial commit

--- Api/articles/script.js
+++ Api/articles/script.js
@@ -5,18 +5,58 @@
 
 
 let API_URL = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=2763ee2fc0524e36a65fc507c1db186a";
+
+
 window.onload = () => {
     onGetNews();
 }
 
 function onGetNews(){
-    document.getElementById('news').innerText = "loading..";
-    
+    document.getElementById('news').innerHTML = "<div> loading..... </div>"
+    // document.getElementById('news').innerHTML = "<h5> Loading.... </h6>";
     fetch(API_URL).then (response => response.json())
-    .then(data => onDisplayData(data))
-    .catch(
-        function error(){
-            console.log(error);
-        }
-    )
+    .then((data) => displayNews(data.articles))
+    .catch(function error(){
+            document.getElementById('news').innerHTML = onGetErrorDiv()
+        })
+}
+
+// function that will display
+
+function displayNews(news) {
+    let news_html = '';
+    news.forEach(function (value){
+        news_html = news_html + 
+        `
+        <div>
+        <h1 > ${value.title}</h1>
+        <div style="font-family: sans-serif; letter-spacing: 1px; line-height: 25px;">
+        <p>"${value.source.name}</p>
+        <img src="${value.urlToImage}" alt="" style="max-width: 100%"> <br>
+        <p> ${value.content} </p> 
+        <p> ${fullFormatDate(value.publishedAt)} </p> 
+        <a href="${value.url}" target="_blank" > Read More ...... </a>
+        </div>
+        </div>
+        `
+        console.log(news_html)
+        console.log(value)
+        document.getElementById('news').innerHTML = news_html;
+    })
+}
+
+function fullFormatDate() {
+    let d = new Date();
+    let date = d;
+    return date.toDateString();
+    
+}
+
+function onGetErrorDiv() {
+    return `
+    <div>
+    <p>    Something Went Wrong! </p>
+    <button onclick="onGetNews()"> Retry </button>
+    </div>
+    `
 }
