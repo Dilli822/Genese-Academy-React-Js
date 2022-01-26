dff43ae3a9aca2cbd81fb0aa14919761fe4f6a32
dilli <dillihangrae@gmail.com>
2022-01-26 21:47:56 +0545

    [added] adds import export class based compoent write inline css and multi line js with react rules how package json lock works ....

--- Genese Academy/React/Learn React/test-app/src/App.js
+++ Genese Academy/React/Learn React/test-app/src/App.js
@@ -1,3 +1,21 @@
 
 
 // This App js is main component in our react app
+
+import React, { Component } from "react";
+import './App.css';
+
+export default class App extends Component{
+    render() {
+        return (
+            // accepts inline css too in react
+            // we write the css in camelCase in react 
+            <div style={{color: '#ffa07a'}}>
+            <div className="firstDiv" style={{margin: 'auto'}}>
+                <p style={{fontSize:30, textAlign:'Center', margin:'1rem'}}> This is Class Based Component </p>
+                {console.log("write js inside the component within curly braces!")}
+            </div>
+            </div>
+        );
+    }
+}
