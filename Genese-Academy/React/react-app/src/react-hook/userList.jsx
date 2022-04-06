af33fac6ecf20da8ebd5c236a6d859bebbbafdc6
dilli <dillihangrae@gmail.com>
2022-04-06 08:10:02 +0545

    [added] adds Delete feature added to Firebase form collection ............

--- Genese-Academy/React/react-app/src/react-hook/userList.jsx
+++ Genese-Academy/React/react-app/src/react-hook/userList.jsx
@@ -17,7 +17,14 @@ import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import {CircularProgress} from "@material-ui/core";
 import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction';
-import Button from "@material-ui/core/Button/Button";
+import IconButton from '@mui/material/IconButton';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 
 
 export default function UserProfileList(){
@@ -25,6 +32,9 @@ export default function UserProfileList(){
 
     const [data, setData] = useState();
     const [loading, setLoading] = useState(true);
+    const [open, setOpen] = useState(false);
+    const [selectedDoc, setSelectedDoc] = useState();
+ 
 
 
 
@@ -34,10 +44,11 @@ export default function UserProfileList(){
         // this variable will extract the data from firebase collection
         const firestore = firebase.firestore();
         const snapshot = await firestore.collection('user-details').get();
-        console.log(snapshot);
+        console.log(snapshot.docs.map(doc => doc)[0].data());
 
         // .data() method converts the binary form of snapshot data of firevase to readbale arrays format(json)
-        return snapshot.docs.map(doc => doc.data());
+        // return snapshot.docs.map(doc => doc.data());
+        return snapshot.docs.map(doc => doc);
     }
 
     useEffect(() => {
@@ -53,6 +64,28 @@ export default function UserProfileList(){
     }, [true]);
 
 
+    // Handle Delete
+    const onDeleteItem = () => {
+      const firestore = firebase.firestore();
+      firestore.collection("user-details").doc("/"+selectedDoc).delete().then(function(){
+        console.log("Document Delete Successfully");
+      }).catch(function(error){
+        console.error("Eror retierieving or deleting the document ", error);
+      })
+      setOpen(false);
+    }
+
+    const handleClose =()=> {
+        setOpen(false)
+    }
+
+    const onSelectedDocForDelete =(id)=> {
+        console.log(id)
+        setSelectedDoc(id);
+        setOpen(true);
+    }
+
+
     return (
         <div>
             <h4> This is User Profile's List! </h4>
@@ -62,7 +95,7 @@ export default function UserProfileList(){
             <List>
                 {data.map((item)=>
                 <ListItem>
-                    <ListItemText primary={item.name} secondary={
+                    <ListItemText primary={item.data().name} secondary={
                                    <React.Fragment>
                                    <Typography
                                      sx={{ display: 'inline' }}
@@ -70,7 +103,7 @@ export default function UserProfileList(){
                                      variant="body2"
                                      color="text.primary"
                                    >
-                                       {item.email}
+                                       {item.data().email}
                                    </Typography>
 
                                    <br />
@@ -79,10 +112,10 @@ export default function UserProfileList(){
 
                                    <br />
                                    Phone Number: &nbsp;
-                                   {item.phone}
+                                   {item.data().phone}
 
                                    <br /> Text: &nbsp;
-                                   {item.text}
+                                   {item.data().text}
 
                                  </React.Fragment>
                     }
@@ -91,7 +124,7 @@ export default function UserProfileList(){
                     {/* Icon for deleting */}
                     <ListItemSecondaryAction>
                         <IconButton edge="end" aria-label="delete">
-                            <DeleteIcon />
+                            <DeleteOutlinedIcon onClick={()=> onSelectedDocForDelete(item.id)} />
                         </IconButton>
                     </ListItemSecondaryAction>
 
@@ -100,6 +133,28 @@ export default function UserProfileList(){
                 )} 
             </List>
                }
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Do you really want to Delete?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+           Alert! You cannot undo this action!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>No</Button>
+          <Button onClick={onDeleteItem} autoFocus>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
         </div>
 
         </div>
